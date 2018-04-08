8f65fcb6d577050a5fae4f08726580836add6b41
John Furtado <jf8846@gmail.com>
2018-04-08 22:42:40 +0000

    recovers jpegs

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(int argc, char *argv[])
 {
     // ensure proper usage
@@ -22,27 +21,60 @@ int main(int argc, char *argv[])
     // counter for JPEG files
     int counter = 0;
 
-    // while not at the end of the file
+    // buffer for memory block (512 bytes)
+    unsigned char buffer[512];
+
+    // string to store file names
+    char name[10];
+
+    // file pointer for JPEGs
+    FILE* jpeg = NULL;
+
+    // while not at the end of the memory card file
     while(!feof(memCard))
     {
-        // buffer for 512 bytes
-        unsigned char buffer[512];
-
-        // read file 1 byte at a time, 512 times
-        fread(&buffer, 1, 512, memCard);
-
-        // if the bytes correspond to a JPEG header
-        if (buffer[0] == 0xff &&
-            buffer[1] == 0xd8 &&
-            buffer[2] == 0xff &&
-            (buffer[3] & 0xf0) == 0xe0)
+        // if not at the last block of memory
+        // store block of memory in buffer
+        if (fread(buffer, 1, sizeof(buffer), memCard) != 0)
         {
-            printf("%d JPEG\n", counter);
-            counter++;
-        }
+            // if the first four bytes correspond to a JPEG header
+            if (buffer[0] == 0xff &&
+                buffer[1] == 0xd8 &&
+                buffer[2] == 0xff &&
+                (buffer[3] & 0xf0) == 0xe0)
+            {
+                // if the file pointer is not NULL close it
+                // it is the start of a new jpeg
+                if (jpeg != NULL)
+                {
+                    fclose(jpeg);
+                }
 
+                // create a new file name using the counter
+                sprintf(name, "%03i.jpg", counter);
+
+                // open the new file
+                jpeg = fopen(name, "w");
+
+                // write to the new file from the buffer
+                fwrite(buffer, 1, sizeof(buffer), jpeg);
+
+                // count the number of JPEGS in memory card file
+                // printf("%d JPEG\n", counter);
+
+                // update counter for next file name
+                counter++;
+            }
+            // else if it is still a JPEG
+            else if (counter > 0)
+            {
+                // write to the new file from the buffer
+                fwrite(buffer, 1, sizeof(buffer), jpeg);
+            }
+        }
     }
 
+    // cloes memory card file process complete
     fclose(memCard);
 
     return 0;
